d0638e5e73c25770b6ef6410a7d3a5709f1f35d7
KavithRashintha <wgkrashintha@gmail.com>
2022-12-22 19:37:50 +0530

    anchorpane loading created

--- src/view/DashboardController.java
+++ src/view/DashboardController.java
@@ -1,24 +1,27 @@
 package view;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class DashboardController {
     public AnchorPane operationAnchorPane;
 
-    public void addBooksOnAction(ActionEvent actionEvent) {
-
+    public void addBooksOnAction(ActionEvent actionEvent) throws IOException {
+        operationAnchorPane.getChildren().add(FXMLLoader.load(getClass().getResource("managementUi/addBooks.fxml")));
     }
 
-    public void updateBooksOnAction(ActionEvent actionEvent) {
-
+    public void updateBooksOnAction(ActionEvent actionEvent) throws IOException {
+        operationAnchorPane.getChildren().add(FXMLLoader.load(getClass().getResource("managementUi/updateBooks.fxml")));
     }
 
-    public void deleteBooksOnAction(ActionEvent actionEvent) {
-
+    public void deleteBooksOnAction(ActionEvent actionEvent) throws IOException {
+        operationAnchorPane.getChildren().add(FXMLLoader.load(getClass().getResource("managementUi/deleteBooks.fxml")));
     }
 
-    public void viewAllOnAction(ActionEvent actionEvent) {
-
+    public void viewAllOnAction(ActionEvent actionEvent) throws IOException {
+        operationAnchorPane.getChildren().add(FXMLLoader.load(getClass().getResource("managementUi/viewAll.fxml")));
     }
 }
