39853cf08e1907b101dfadee3e334e2114b493b7
KavithRashintha <wgkrashintha@gmail.com>
2023-01-07 16:55:24 +0530

    Data loading function in the update created

--- src/view/UpdateBooksController.java
+++ src/view/UpdateBooksController.java
@@ -1,7 +1,14 @@
 package view;
 
+import database.DBConnection;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UpdateBooksController {
     public TextField bookId;
@@ -11,6 +18,21 @@ public class UpdateBooksController {
     public TextField numberOfCopies;
 
     public void updateBookOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void loadBookOnAction(ActionEvent actionEvent) throws SQLException, ClassNotFoundException {
+        String sql="SELECT * FROM books WHERE id=?";
+        Connection connection= DBConnection.getInstance().getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1 , bookId.getText());
+        ResultSet resultSet = statement.executeQuery();
+        if(resultSet.next()){
+            bookName.setText(resultSet.getString(2));
+            bookAuthor.setText(resultSet.getString(3));
+            publishedYear.setText(String.valueOf(resultSet.getInt(4)));
+            numberOfCopies.setText(String.valueOf(resultSet.getInt(5)));
+        }
 
     }
 }
